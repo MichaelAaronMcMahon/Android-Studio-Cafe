ae8fa5381af4d141e3ef61926d1ab65208b502a6
MichaelAaronMcMahon <michaelaaronmcmahon@gmail.com>
2024-04-17 16:45:57 -0400

    Starting on remove (crashes)

--- app/src/main/java/com/example/rutgerscafe/CurrentOrderActivity.java
+++ app/src/main/java/com/example/rutgerscafe/CurrentOrderActivity.java
@@ -1,9 +1,13 @@
 package com.example.rutgerscafe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,19 +21,60 @@ public class CurrentOrderActivity extends AppCompatActivity {
     private Button bt_placeOrder;
     private ArrayList<MenuItem> listItems;
     private ArrayAdapter<MenuItem> currentAdapter;
+    private MenuItem removeItem;
+    OrderData orderData = OrderData.getInstance();
+    public void setLv_currentOrder(){
+        listItems = new ArrayList<MenuItem>(Arrays.asList(orderData.getCurrentOrder().getMenuList()));
+        currentAdapter = new ArrayAdapter<MenuItem>(this,
+                android.R.layout.simple_list_item_single_choice, listItems);
+        lv_currentOrder.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        lv_currentOrder.setAdapter(currentAdapter);
+    }
+
+    public void makeToast(){
+        Toast.makeText(this,
+                removeItem.toString(),
+                Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        OrderData orderData = OrderData.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_order);
         lv_currentOrder = findViewById(R.id.lv_currentOrder);
         bt_placeOrder = findViewById(R.id.bt_placeOrder);
         bt_removeItem = findViewById(R.id.bt_removeItem);
-        listItems = new ArrayList<MenuItem>(Arrays.asList(orderData.getCurrentOrder().getMenuList()));
-        currentAdapter = new ArrayAdapter<MenuItem>(this,
-                android.R.layout.simple_list_item_multiple_choice, listItems);
-        lv_currentOrder.setAdapter(currentAdapter);
+        setLv_currentOrder();
+
+        /*lv_currentOrder.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //removeItem = (MenuItem) parent.getItemAtPosition(position);
+                //makeToast();
+                orderData.getCurrentOrder().remove((MenuItem) parent.getItemAtPosition(position));
+                setLv_currentOrder();
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });*/
+        lv_currentOrder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                removeItem = (MenuItem) parent.getItemAtPosition(position);
+                makeToast();
+            }
+        });
+        bt_removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                orderData.getCurrentOrder().remove(removeItem);
+                //setLv_currentOrder();
+                listItems = new ArrayList<MenuItem>(Arrays.asList(orderData.getCurrentOrder().getMenuList()));
+            }
+        });
 
     }
 }
